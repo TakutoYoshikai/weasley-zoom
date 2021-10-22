df7396231c4423681373a0869aafd28e927d2bf9
lucky <takuto.yoshikai@gmail.com>
2021-10-22 14:23:23 +0900

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# weasley-zoom
+This is a tool to close ZOOM window at any time. 
+
+### Usage
+**install**
+```bash
+git clone https://github.com/TakutoYoshikai/weasley-zoom.git
+cd weasley-zoom
+pip3 install -r requirements.txt
+```
+
+**set timer**
+```bash
+python3 weasley.py <HOUR> <MINUTE>
+# Example: close ZOOM window at 13:15.
+# python3 weasley.py 13:15 
+```
+
+### LICENSE
+MIT LICENSE
---
+++ weasley.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+import datetime
+import sys
+
+hour = int(sys.argv[1])
+minute = int(sys.argv[2])
+
+while True:
+    now = datetime.datetime.now()
+    if now.hour == hour and now.minute == minute:
+        pyautogui.keyDown("command")
+        pyautogui.press("w")
+        pyautogui.keyUp("command")
+        time.sleep(0.4)
+        pyautogui.press("enter")
+        sys.exit()
+    time.sleep(1)
