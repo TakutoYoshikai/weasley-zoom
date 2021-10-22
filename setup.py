8c2676316e689d1c2936b1039763b32b0e85c5e3
lucky <takuto.yoshikai@gmail.com>
2021-10-22 15:17:10 +0900

    pip

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages = find_packages(),
     entry_points={
         "console_scripts": [
-            "weasley-zoom = weasley:main",
+            "weasley-zoom = weasley.weasley:main",
         ]
     }
 )
